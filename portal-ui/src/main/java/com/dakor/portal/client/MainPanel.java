95da9afb81c597d6f05ab8f273ae98c0c30c8b9c
Dmitry Korobeynikov <dakor@meta.ua>
2013-08-04 22:55:34 +0300

    second commit

--- portal-ui/src/main/java/com/dakor/portal/client/MainPanel.java
+++ portal-ui/src/main/java/com/dakor/portal/client/MainPanel.java
@@ -2,20 +2,19 @@ package com.dakor.portal.client;
 
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.HTMLPanel;
-import com.google.gwt.user.client.ui.Widget;
 
 /**
  *
  */
-public class MainPanel extends Widget {
+public class MainPanel extends Composite {
     interface MainPanelUiBinder extends UiBinder<HTMLPanel, MainPanel> {
     }
 
     private static MainPanelUiBinder ourUiBinder = GWT.create(MainPanelUiBinder.class);
 
     public MainPanel() {
-        ourUiBinder.createAndBindUi(this);
-
+        initWidget(ourUiBinder.createAndBindUi(this));
     }
 }
